d90962f0edba19666f8fe4a8ccaccf42cafbe96b
mangachh <mangachh@gmail.com>
2022-12-06 16:18:49 +0100

    labels displaying correctly

--- models/hr.py
+++ models/hr.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class Employee(models.Model):
     _inherit = "hr.employee"
 
-    formative_actions = fields.Many2many('training.formative_action', string="Courses")
+    formative_actions = fields.Many2many('training.formative_action', string="Employee Formative Actions")
